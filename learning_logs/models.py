2b91db567ffedb7213a4932b1bb5b9362acfafd4
pragiark <pragiark@wp.pl>
2019-10-14 00:15:32 +0200

    Try add Entrier (ForeignKey error)

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,6 +5,21 @@ class Topic(models.Model):
     text = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add = True)
 
-def __str__(self):
-    """Return presentation of models"""
-    return self.text
+
+    def __str__(self):
+        """Return presentation of models"""
+        return self.text
+
+
+class Entry(models.Model):
+    """Information about learning"""
+ #   topic = models.ForeignKey(Topic, on_delete=models.CASCADE,)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Return presentation of models"""
+        return self.text[:50] + "..."
